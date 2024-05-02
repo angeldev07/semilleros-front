35ed7191c78fdabcf327855829c363b98fa7e4b3
Yieison <76229560+Yieison@users.noreply.github.com>
2024-05-02 07:05:55 -0500

    Create normatividad list component

---
+++ src/app/normatividad/normatividad-list/normatividad-list.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-normatividad-list',
+  template: `
+    <div>
+      <h1>List of Normatividad</h1>
+      <ul>
+        <!-- Each normatividad item will be rendered here -->
+        <li *ngFor="let normatividad of normatividades">
+          {{ normatividad.name }}
+        </li>
+      </ul>
+    </div>
+  `,
+  styleUrls: ['./normatividad-list.component.css']
+})
+export class NormatividadListComponent implements OnInit {
+  normatividades = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Load normatividades from a service
+  }
+}
