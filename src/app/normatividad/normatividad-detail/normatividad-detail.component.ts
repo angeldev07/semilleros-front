5c65bb8e111d5ba56ae4fe1079c4a283cd169c7b
Yieison <76229560+Yieison@users.noreply.github.com>
2024-05-02 07:08:58 -0500

    Create normatividad detail component

---
+++ src/app/normatividad/normatividad-detail/normatividad-detail.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-normatividad-detail',
+  template: `
+    <div>
+      <h1>Normatividad Details</h1>
+      <p>{{ normatividad.name }}</p>
+      <p>{{ normatividad.description }}</p>
+    </div>
+  `,
+  styleUrls: ['./normatividad-detail.component.css']
+})
+export class NormatividadDetailComponent implements OnInit {
+  normatividad: any = {};
+
+  constructor(private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      // Fetch the normatividad details using the id from params
+    });
+  }
+}
