11ea69e0b4ae2e3cfb3b60645c8c84b29a2874b0
barkoHub <brayanalexanderbc@ufps.edu.co>
2024-05-02 21:28:57 -0500

    ReportePDF

--- src/app/reportes/reportes.component.ts
+++ src/app/reportes/reportes.component.ts
@@ -1,36 +1,54 @@
 import { CommonModule } from '@angular/common';
 import { ChangeDetectionStrategy, Component, type OnInit } from '@angular/core';
-
+import {jsPDF} from 'jspdf';
 @Component({
   selector: 'app-reportes',
   standalone: true,
-  imports: [
-    CommonModule,
-  ],
+  imports: [CommonModule],
   templateUrl: './reportes.component.html',
-  styles: `
-  h2 {
-      text-align: center;
-  }
-  .post {
-      border: 1px solid #ccc;
-      padding: 10px;
-      margin-bottom: 10px;
-  }
-  .report {
-      border: 1px solid #ccc;
-      padding: 10px;
-      margin-bottom: 10px;
-  }
-  `,
+  styleUrls: ['./reportes.component.css'], // Agregar la ruta del archivo CSS
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ReportesComponent implements OnInit {
+export class ReportesComponent {
+  public cant_publicaciones = 10; // cantidad de publicaciones x mes
+  public alumnosMes = 30; // cantidad de alumnos matriculados en el mes
+  public titulo = "Reportes"; // titulo principal
 
-  public cant_publicaciones = 10;   //cantidad de publicaciones x mes
-  public alumnosMes = 30;           //cantidad de alumnos matriculados en el mes
-  public titulo = "Reportes";       //titulo principal
+  generarAlumnos() {
+    const doc = new jsPDF();
+    // Agregar contenido al PDF
+    doc.setFontSize(18);
+    doc.text('Reporte de Alumnos Matriculados', 20, 20);
+    doc.setFontSize(12);
+    doc.text(`Cantidad de alumnos: ${this.alumnosMes}`, 20, 40);
+    // Guardar el PDF
+    doc.save('reporte-alumnos.pdf');
+  }
 
-  ngOnInit(): void { }
+  generarPublicaciones() {
+    const doc = new jsPDF();
+    // Agregar contenido al PDF
+    doc.setFontSize(18);
+    doc.text('Reporte de Publicaciones del mes', 20, 20);
+    doc.setFontSize(12);
+    doc.text(`Cantidad de publicaciones: ${this.cant_publicaciones}`, 20, 40);
+    // Guardar el PDF
+    doc.save('reporte-publicaciones.pdf');
+  }
+
+  generarGeneral() {
+    const doc = new jsPDF();
+    // Agregar contenido al PDF
+    doc.setFontSize(18);
+    doc.text('Reporte de Alumnos Matriculados en el mes', 20, 20);
+    doc.setFontSize(12);
+    doc.text(`Cantidad de alumnos: ${this.alumnosMes}`, 20, 40);
+    doc.setFontSize(18);
+    doc.text('Reporte de Publicaciones del mes', 20, 60);
+    doc.setFontSize(12);
+    doc.text(`Cantidad de publicaciones: ${this.cant_publicaciones}`, 20, 80);
+    // Guardar el PDF
+    doc.save('reporte-general.pdf');
+  }
 
 }
